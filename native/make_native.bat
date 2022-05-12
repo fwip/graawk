cd5682d7cbe57da1f38d41ce381ce1b116a5ce00
Jemma Nelson <Pink.fwip@gmail.com>
2022-05-11 21:04:04 -0700

    Update make_native.bat

--- native/make_native.bat
+++ native/make_native.bat
@@ -4,4 +4,4 @@ IF [[ "%SL_BUILD_NATIVE%" == "false" "]]" (
   echo "Skipping the native image build because SL_BUILD_NATIVE is set to false."
   exit "0"
 )
-"$JAVA_HOME"/bin/native-image "--macro:truffle" "--no-fallback" "--initialize-at-build-time=com.oracle.truffle.sl,org.antlr.v4" "-cp" "%CD%.\language\target\simplelanguage.jar:%CD%.\launcher\target\sl-launcher.jar" "com.oracle.truffle.sl.launcher.SLMain" "slnative"
+"%JAVA_HOME%"/bin/native-image "--macro:truffle" "--no-fallback" "--initialize-at-build-time=com.oracle.truffle.sl,org.antlr.v4" "-cp" "..\language\target\simplelanguage.jar:..\launcher\target\sl-launcher.jar" "com.oracle.truffle.sl.launcher.SLMain" "slnative"
